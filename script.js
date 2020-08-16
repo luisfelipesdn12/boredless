b9444640329d98de52db81bd32a71bbf03c71660
unknown <luisfelipesdn12@gmail.com>
2020-08-16 13:28:40 -0300

    Add previous and better logic

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function showLoadingMessageIfIsNotLoaded() {
+    if (document.getElementById("activity").innerHTML == "") {
+        document.getElementById("type-of-activity").innerHTML = "Loading...";
+        document.getElementById("activity").innerHTML = "If it is take longer, refresh the page.";
+    }
+}
+
 function changeBackgroundColor(c1, c2) {
     document.documentElement.style.setProperty('--first-color-to-gradient', c1);
     document.documentElement.style.setProperty('--second-color-to-gradient', c2);
@@ -16,8 +23,68 @@ function setGradientRandomly() {
         .catch(err => console.log(err));
 }
 
-function populateWithANewSuggestion() {
-    fetch("https://www.boredapi.com/api/activity")
+allSuggestionKeys = [];
+index = 0;
+
+async function getRandomActivityKey() {
+    return await fetch("https://www.boredapi.com/api/activity")
+                    .then(res => res.json())
+                    .then(activity => activity.key);
+}
+
+function putANewKeyToTheList() {
+    getRandomActivityKey()
+        .then(key => allSuggestionKeys.push(key));
+}
+
+async function nextSuggestion() {
+    index = index + 1;
+
+    if (index > 0) {
+        document.getElementById("previous").style.display = "block";
+
+        if (document.getElementById("generate-centered") !== null) {
+            document.getElementById("generate-centered").id = "generate";
+        }
+        
+    }
+
+    if (index >= allSuggestionKeys.length) {
+        await getRandomActivityKey()
+                .then(key => allSuggestionKeys.push(key));
+    }
+    
+    return allSuggestionKeys[index];
+}
+
+function previousSuggestion() {
+    if (index !== 0) {
+        index = index - 1;
+    }
+    if (index === 0) {
+        document.getElementById("previous").style.display = "none";
+        document.getElementById("generate").id = "generate-centered";
+    }
+    return allSuggestionKeys[index];
+}
+
+async function initialPopulation() {
+    await getRandomActivityKey()
+        .then(key => allSuggestionKeys.push(key));
+    populateWithAKeySuggestion(allSuggestionKeys[0]);
+}
+
+function populateWithTheNextSuggestion() {
+    nextSuggestion()
+        .then(key => populateWithAKeySuggestion(key));
+}
+
+function populateWithThePreviousSuggestion() {
+    populateWithAKeySuggestion(previousSuggestion());
+}
+
+function populateWithAKeySuggestion(key) {
+    fetch("https://www.boredapi.com/api/activity?key=" + key)
         .then(res => res.json())
         .then(activity => {
             console.log("All my response:");
